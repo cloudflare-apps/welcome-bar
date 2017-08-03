324aff8e603357dc9b807a824d5d18011f826900
Teffen Ellis (Eric) <teffen@nirri.us>
2017-08-03 15:26:15 -0700

    Flesh out app refactor.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-To build, run `npm install; bower install; gulp build`
-To develop locally, `npm install; bower install; gulp`
+# Welcome Bar
+
+## Setup
+
+Install the dependencies with `npm install`
+
+## Usage
+
+- `npm start` Watches for file changes and compile them.
+- `npm run build` Compiles your JavaScript and CSS once for release.
+- `npm run lint` Checks JavaScript formatting and attempts to fix any errors.
+
+## Details
+
+#### `source/app.js`
+
+This is where the magic happens. Your app starts here.
+
+#### `source/app.css`
+
+Styles for your app.
+
+#### `install.json`
+
+This is where all the <a href="https://www.cloudflare.com/apps/developer/docs/install-json">installer options</a> are added for the app.
+
+<a href="http://install.json.is/">Syntax can be tricky</a>, so be sure to double check it.
+
+#### `media/**`
+
+An directory for icons, tile images, and screenshots.
+
+[Download <code class="inline">media-templates.sketch</code>](https://github.com/CloudflareApps/MediaTemplates/raw/master/media-templates.sketch)
+
+### Troubleshooting
+
+- <a href="https://www.cloudflare.com/apps/developer/docs/getting-started">The Cloudflare developer documentation</a> for examples and API usage.
+
+<a href="https://www.cloudflare.com/apps/[[YOUR APP ALIAS]]/install?source=button">
+  <img
+    src="https://install.cloudflareapps.com/install-button.png"
+    alt="Install [[YOUR APP NAME]] with Cloudflare"
+    border="0"
+    width="150">
+</a>
---
+++ source/app.js
@@ -0,0 +1,252 @@
+(function () {
+  'use strict'
+
+  if (!window.addEventListener) return // Check for IE9+
+
+  let options = INSTALL_OPTIONS
+  const VISIBILITY_ATTRIBUTE = 'data-cf-welcome-bar-visibility'
+  const documentElementOriginallyPositionStatic = window.getComputedStyle(document.documentElement).position === 'static'
+
+  const element = document.createElement('cloudflare-app')
+  element.setAttribute('app', 'welcome-bar')
+
+  const htmlStyle = document.createElement('style')
+  document.head.appendChild(htmlStyle)
+
+  const elementStyle = document.createElement('style')
+  document.head.appendChild(elementStyle)
+
+  function getMaxZIndex () {
+    var max = 0
+    var elements = document.getElementsByTagName('*')
+
+    Array.prototype.slice.call(elements).forEach(element => {
+      var zIndex = parseInt(document.defaultView.getComputedStyle(element).zIndex, 10)
+
+      max = zIndex ? Math.max(max, zIndex) : max
+    })
+
+    return max
+  }
+
+  function setPageStyles () {
+    setHTMLStyle()
+    setFixedElementStyles()
+  }
+
+  function setHTMLStyle () {
+    if (!document.body) return
+
+    let style = ''
+
+    if (documentElementOriginallyPositionStatic && isShown()) {
+      style = `
+        html {
+          position: relative;
+          top: ${element.clientHeight}px;
+        }
+      `
+    }
+
+    htmlStyle.innerHTML = style
+  }
+
+  function setFixedElementStyles () {
+    function removeTopStyle (node) {
+      const currentStyle = node.getAttribute('style')
+      if (!currentStyle) return
+
+      node.setAttribute('style', currentStyle.replace(/top[^]+?/g, ''))
+    }
+
+    // Cache this to minimize potential repaints.
+    const elementHeight = element.clientHeight
+
+    // Find fixed position nodes to adjust.
+    const allNodes = document.querySelectorAll('*:not([app="welcome-bar"]):not([data-cfapps-welcome-bar-adjusted-fixed-element-original-top])')
+
+    Array.prototype.forEach.call(allNodes, node => {
+      const computedStyle = window.getComputedStyle(node)
+      const boundingClientRect = node.getBoundingClientRect()
+
+      const isSticky = computedStyle.position === 'sticky'
+      const isFixed = computedStyle.position === 'fixed'
+      const isBottomFixed = computedStyle.bottom === '0px' && boundingClientRect.bottom === window.innerHeight && boundingClientRect.top >= elementHeight
+
+      if (INSTALL_ID === 'preview' && node.nodeName === 'IFRAME' && node.src.indexOf('https://embedded.cloudflareapps.com') !== -1) {
+        // HACK: Improves mobile experience by omitting preview notice.
+        return
+      }
+
+      if ((isFixed || isSticky) && !isBottomFixed) {
+        const {top} = boundingClientRect
+        const styleTop = parseInt(computedStyle.top, 10)
+
+        if (isSticky || (top === styleTop && top <= elementHeight)) {
+          node.setAttribute('data-cfapps-welcome-bar-adjusted-fixed-element-original-top', top)
+        }
+      }
+    })
+
+    // Adjust them.
+    const adjustedNodes = document.querySelectorAll('[data-cfapps-welcome-bar-adjusted-fixed-element-original-top]')
+
+    Array.prototype.forEach.call(adjustedNodes, node => {
+      removeTopStyle(node)
+
+      const computedStyle = window.getComputedStyle(node)
+      const isFixedOrSticky = computedStyle.position === 'fixed' || computedStyle.position === 'sticky'
+
+      if (isFixedOrSticky && isShown() && elementHeight > 0) {
+        const newTop = (parseInt(computedStyle.top, 10) || 0) + elementHeight
+        node.style.top = newTop + 'px'
+      }
+    })
+  }
+
+  function isShown () {
+    return document.documentElement.getAttribute(VISIBILITY_ATTRIBUTE) === 'visible'
+  }
+
+  function hideWelcomeBar () {
+    document.documentElement.setAttribute(VISIBILITY_ATTRIBUTE, 'hidden')
+    element.removeAttribute('data-slide-animation')
+
+    try {
+      window.localStorage.cfAlertBarOptions = JSON.stringify(options)
+    } catch (e) {}
+
+    setPageStyles()
+
+    if (INSTALL_ID === 'preview') {
+      setTimeout(() => {
+        document.documentElement.setAttribute(VISIBILITY_ATTRIBUTE, 'visible')
+        setPageStyles()
+        updateAnimation()
+      }, 2000)
+    }
+  }
+
+  function cancelAnimation () {
+    element.removeEventListener('transitionend', hideWelcomeBar)
+    element.removeAttribute('data-slide-animation')
+  }
+
+  function updateAnimation () {
+    if (!options.behavior.automaticallyHide) {
+      cancelAnimation()
+      return
+    }
+
+    element.addEventListener('transitionend', hideWelcomeBar)
+    element.addEventListener('mouseover', cancelAnimation)
+    element.addEventListener('click', cancelAnimation)
+
+    // Note: This ensures that all browsers trigger the transition on load.
+    window.requestAnimationFrame(() => {
+      element.setAttribute('data-slide-animation', '')
+
+      window.requestAnimationFrame(() => {
+        element.setAttribute('data-slide-animation', 'complete')
+      })
+    })
+  }
+
+  function updateElementStyle () {
+    elementStyle.innerHTML = `
+      cloudflare-app[app="welcome-bar"] {
+        background-color: ${options.theme.backgroundColor};
+        color: ${options.theme.textColor};
+      }
+
+      @media (min-width: 768px) {
+        cloudflare-app[app="welcome-bar"][data-style="prominent"] .alert-cta-button {
+          background-color: ${options.theme.buttonBackgroundColor} !important;
+          color: ${options.theme.buttonTextColor === 'auto' ? options.theme.backgroundColor : options.theme.buttonTextColor} !important;
+        }
+      }
+    `
+
+    element.setAttribute('data-style', options.theme.style)
+  }
+
+  function updateElement () {
+    let hasSeenAlert = false
+
+    try {
+      hasSeenAlert = window.localStorage.cfAlertBarOptions === JSON.stringify(options)
+    } catch (e) {}
+
+    if (hasSeenAlert && INSTALL_ID !== 'preview') return
+
+    updateElementStyle()
+    element.innerHTML = ''
+    element.style.zIndex = getMaxZIndex() + 1
+
+    const message = document.createElement('alert-message')
+    message.innerHTML = options.message
+
+    element.appendChild(message)
+
+    if (options.cta.show) {
+      const ctaButton = document.createElement('a')
+      ctaButton.className = 'alert-cta-button'
+      ctaButton.textContent = options.cta.label.trim() || 'More info'
+
+      if (options.cta.newWindow) ctaButton.target = '_blank'
+
+      if (options.cta.url) ctaButton.href = options.cta.url
+
+      message.appendChild(ctaButton)
+    }
+
+    if (options.behavior.showCloseButton) {
+      const dismissButton = document.createElement('alert-dismiss')
+
+      dismissButton.setAttribute('role', 'button')
+      dismissButton.textContent = '×'
+
+      dismissButton.addEventListener('click', hideWelcomeBar)
+
+      element.appendChild(dismissButton)
+    }
+
+    document.documentElement.setAttribute(VISIBILITY_ATTRIBUTE, 'visible')
+
+    updateAnimation()
+  }
+
+  function bootstrap () {
+    document.body.appendChild(element)
+
+    updateElement()
+
+    window.requestAnimationFrame(setPageStyles)
+    window.addEventListener('resize', setPageStyles)
+  }
+
+  // This code ensures that the app doesn't run before the page is loaded.
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', bootstrap)
+  } else {
+    bootstrap()
+  }
+
+  // INSTALL_SCOPE is an object that is used to handle option changes without refreshing the page.
+  window.INSTALL_SCOPE = {
+    setOptions (nextOptions) {
+      options = nextOptions
+
+      updateElement()
+      setPageStyles()
+    },
+    setTheme (nextOptions) {
+      const themeStyleChanged = nextOptions.theme.style !== options.theme.style
+      options = nextOptions
+
+      updateElementStyle()
+
+      if (themeStyleChanged) setPageStyles()
+    }
+  }
+}())
